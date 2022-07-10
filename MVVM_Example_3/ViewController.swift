241404b7b5a7f65bfb7f349f46b3eecbb16cf987
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-10 18:27:15 +0300

    Initial commit

---
+++ MVVM_Example_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Example_3
+//
+//  Created by test on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
